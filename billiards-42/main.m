da6f3a31b664dc52f77d65e40d8babdee36525b8
Admin <Admin@Admins-Mac.local>
2013-09-19 11:26:37 +0300

    Initial Commit

---
+++ billiards-42/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  billiards-42
+//
+//  Created by Admin on 9/19/13.
+//  Copyright SalkoDev 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
